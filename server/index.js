bef2b76d17d208c54a54d60b88ca355ad6fba026
Megan Astraus <nicka@NICK-ASTRAUSs-MacBook-Pro.local>
2023-06-30 22:21:31 -0700

    index changes for deploy

--- server/index.js
+++ server/index.js
@@ -2,17 +2,23 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const pizzaRouter = require("./routers/pizzaRoutes");
 const toppingsRouter = require("./routers/toppingsRoutes");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "..", "client", "build")));
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/pizzas", pizzaRouter);
 app.use("/toppings", toppingsRouter);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
+});
 
 module.exports = app;
