e079a70f5b3737e11a11fef542c299d6a2992321
Megan Astraus <nicka@NICK-ASTRAUSs-MacBook-Pro.local>
2023-06-30 17:53:52 -0700

    cleanup, make readme

--- server/db/migrations/20230621220141_pizza_names.js
+++ server/db/migrations/20230621220141_pizza_names.js
@@ -1,10 +1,10 @@
-exports.up = function(knex) {
-    return knex.schema.createTable('pizza_names',(table) => {
-      table.increments('id').primary();
-      table.string('pizza_name');
-    })
-  };
-  
-  exports.down = function(knex) {
-    return knex.schema.dropTable('pizza_names');
-  };
+exports.up = function (knex) {
+  return knex.schema.createTable("pizza_names", (table) => {
+    table.increments("id").primary();
+    table.string("pizza_name");
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable("pizza_names");
+};
